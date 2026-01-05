64a07e2af99d346e2417e43d0b6967a820ebd7df
asif-raza01 <155326429+asif-raza01@users.noreply.github.com>
2026-01-05 14:01:18 +0530

    Create node.js

---
+++ JSconcepts/node.js
@@ -0,0 +1,6 @@
+// Node js is Javascript outside C/C++ on the inside
+
+// 1.V8- The Javascript engine (from Chrome)
+// 2.libuv-C library for event loop,thread pool,async I/O
+// 3.C++ bindings-Glue between JS and low level Os opperations
+// 4.Node.js core(JS + C++) -Provide fs,http,net,etc
