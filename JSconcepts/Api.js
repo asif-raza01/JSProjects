e63c510659afd47ba4e76e19df22058eb7535266
asif-raza01 <155326429+asif-raza01@users.noreply.github.com>
2026-01-06 02:59:23 +0530

    Create Api.js

---
+++ JSconcepts/Api.js
@@ -0,0 +1,9 @@
+//earlier there were SOAP APIs but they slow and heavy why beacause they used XML which contaions a lot of meta data along with valuable data
+//REST => Represtational State Transfer
+//so REST APIs came in picture they were flexible  fast able to use different formats eg JSON no heavy data 
+//stateful means it remembers the states so it is fast but but it is a single point failure possibility
+//stateless it keeps track of states of remembers them specifically scalable 
+//idempotent APIs get/put/delete only works one if you make get request it will give the same item
+//non idempotent post because at every call generation of new object takes place
+//URI uniform resource identifier uniquely identifies
+//URL unifrom resource locator domain name that we use example facebook.com
