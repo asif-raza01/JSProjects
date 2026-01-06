2a33555140ee86aee674690a35778a9220bf5ea6
asif-raza01 <155326429+asif-raza01@users.noreply.github.com>
2026-01-06 23:35:12 +0530

    Add files via upload

---
+++ Backend/HTTPserver/server.js
@@ -0,0 +1,12 @@
+const http=require('http')
+
+const port=3000;
+
+const server=http.createServer((req,res)=>{
+    if(req.url=='/' && req.method=='GET'){
+        res.writeHead(200,{'Content-Type':'text/plain'})
+        res.end("Welcome to the homepage!")
+    }
+})
+
+server.listen(port);
